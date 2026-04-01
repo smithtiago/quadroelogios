b5cbe319b4f1b60396bf3db4acd03df9e059cade
smithtiago <tiagosmithprado@gmail.com>
2026-04-01 14:19:59 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CONFIG = {
-  endpointEnvio: "https://default835ec4be00c0452e8dc00552b036af.42.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/a9916317da0945d186f784a77733ea69/triggers/manual/paths/invoke?api-version=1"
+  endpointEnvio: "https://default835ec4be00c0452e8dc00552b036af.42.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/a9916317da0945d186f784a77733ea69/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=HfNj60J8fw2V4d8EJgVUJDvcO22lZ73lq-ap8nS87Oo"
 };

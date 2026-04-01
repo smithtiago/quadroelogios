b3dfe7debdd88380783804ecc2ae72a5d085d316
smithtiago <tiagosmithprado@gmail.com>
2026-04-01 14:04:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.CONFIG = {
-  usarMock: true,
-  endpointEnvio: "",
-  endpointLeitura: ""
+  endpointEnvio: "https://default835ec4be00c0452e8dc00552b036af.42.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/a9916317da0945d186f784a77733ea69/triggers/manual/paths/invoke?api-version=1"
 };
